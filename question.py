ec5f7f8e37bdcfc594bbc22e7591985cbeb73b07
caninecrew <samuelleerumbley@gmail.com>
2025-03-31 15:04:55 -0500

    Add methods to validate answers and shuffle answer options in Question class

--- question.py
+++ question.py
@@ -1,8 +1,18 @@
+import random
 class Question:
     def __init__(self, questionText, correctAnswer, incorrectAnswers):
         self.questionText = questionText
         self.correctAnswer = correctAnswer
         self.incorrectAnswers = incorrectAnswers
         self.allAnswers = [correctAnswer] + incorrectAnswers
+    
+    def isCorrect(self, answer):
+        return answer == self.correctAnswer
+    
+    def getShuffledAnswers(self):
+        shuffled = self.allAnswers.copy()
+        random.shuffle(shuffled)
+        return shuffled
+    
 
     
