8807051b3cd4cecd2a9261b989859a36f5450100
caninecrew <samuelleerumbley@gmail.com>
2025-03-31 10:42:27 -0500

    Move quiz questions to a new question.py file for better organization

--- database.py
+++ database.py
@@ -9,16 +9,7 @@ import random as rand
 
 varCorrectAnswer = 0
 
-# format: question : [correct answer, incorrect answer, incorrect answer, incorrect answer],
-questions = {
-            "What is the capital of Spain?" : ["Madrid", "Barcelona", "Valencia", "Seville"],
-            "Who was the second Vice President of the United States?" : ["Thomas Jefferson", "John Adams", "Aaron Burr", "George Clinton"],
-            "What was the name of the son of Adam and Eve who killed his brother?" : ["Cain", "Abel", "Seth", "Noah"],
-            "What does the TV network abbreviation CBS stand for?" : ["Columbia Broadcasting System", "Community Broadcasting Station", "Cable Broadcasting System", "Creative Broadcast Studios"],
-            "How many of Henry VIII's wives were executed?" : ["two", "one", "three", "four"],
-            "What is the name of the Mayflower's sister ship?" : ["Speedwell", "Camellia", "Lilac", "Sweetbriar"],
-            "Where is the world's largest McDonald's located?" : ["Orlando, Flordia", "Paris, France", "London, England", "New York City, New York"],
-}
+
 
 questionsList = list(questions.keys()) # creates a list of all the keys to the dictionary (questions)
 questionsGame = [] # the questions chosen for the game (initialize variable)
---
+++ question.py
@@ -0,0 +1,10 @@
+# format: question : [correct answer, incorrect answer, incorrect answer, incorrect answer],
+questions = {
+            "What is the capital of Spain?" : ["Madrid", "Barcelona", "Valencia", "Seville"],
+            "Who was the second Vice President of the United States?" : ["Thomas Jefferson", "John Adams", "Aaron Burr", "George Clinton"],
+            "What was the name of the son of Adam and Eve who killed his brother?" : ["Cain", "Abel", "Seth", "Noah"],
+            "What does the TV network abbreviation CBS stand for?" : ["Columbia Broadcasting System", "Community Broadcasting Station", "Cable Broadcasting System", "Creative Broadcast Studios"],
+            "How many of Henry VIII's wives were executed?" : ["two", "one", "three", "four"],
+            "What is the name of the Mayflower's sister ship?" : ["Speedwell", "Camellia", "Lilac", "Sweetbriar"],
+            "Where is the world's largest McDonald's located?" : ["Orlando, Flordia", "Paris, France", "London, England", "New York City, New York"],
+}
